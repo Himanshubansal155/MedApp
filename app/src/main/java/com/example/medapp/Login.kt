9073fd998ed633ac9600a652000bae9124ab9b5f
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-26 15:52:53 +0530

    Implemented the login class

--- app/src/main/java/com/example/medapp/Login.kt
+++ app/src/main/java/com/example/medapp/Login.kt
@@ -1,4 +1,24 @@
 package com.example.medapp
 
-class Login {
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.medapp.databinding.ActivityMainBinding
+
+class Login : AppCompatActivity(){
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        val animation : AnimationDrawable = binding.root.background as AnimationDrawable
+        animation.setEnterFadeDuration(1500)
+        animation.setExitFadeDuration(3000)
+        animation.start()
+    }
+
 }
