fde5ebcab2e902655b6f7adcd132c1d1153f39d1
farddinkhan730 <farddin.khan_cs19@gla.ac.in>
2021-07-29 10:38:04 +0530

    fk

--- app/src/main/java/com/example/medapp/EditReminder.java
+++ app/src/main/java/com/example/medapp/EditReminder.java
@@ -1,24 +1,49 @@
 package com.example.medapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.res.ColorStateList;
+import android.app.DatePickerDialog;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
 public class EditReminder extends AppCompatActivity {
-    Button afterDinnerButton, afterBreakfastButton, afterLunchButton, beforeDinnerButton, beforeBreakfastButton, beforeLunchButton;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    Button donereminder;
+    EditText mname;
+    TextView t1,mo;
+    DatePickerDialog.OnDateSetListener setListener1,setListener2;
+    private int selectedId;
+    private Stack<String> ll;
+    private  boolean check=false;
+
+    Button afterDinnerButton, afterBreakfastButton, afterLunchButton, beforeDinnerButton, beforeBreakfastButton, beforeLunchButton,confirm_butt;
     boolean afterDinnerSelected = false;
     boolean afterLunchSelected = false;
     boolean afterBreakfastSelected = false;
@@ -27,6 +52,7 @@ public class EditReminder extends AppCompatActivity {
     boolean beforeBreakfastSelected = false;
     RadioButton genderradioButton, lastButtonReference;
     RadioGroup radioGroup;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -40,6 +66,13 @@ public class EditReminder extends AppCompatActivity {
         android.graphics.drawable.Drawable background = EditReminder.this.getResources().getDrawable(R.drawable.color);
         getWindow().setBackgroundDrawable(background);
 
+        donereminder=findViewById(R.id.done);
+        mname=findViewById(R.id.medname);
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        mo=findViewById(R.id.monthedit);
+        t1=findViewById(R.id.daily);
+
         afterDinnerButton = findViewById(R.id.afterDinnerButton);
         afterBreakfastButton = findViewById(R.id.afterBreakfastButton);
         afterLunchButton = findViewById(R.id.afterLunchButton);
@@ -47,28 +80,149 @@ public class EditReminder extends AppCompatActivity {
         beforeBreakfastButton = findViewById(R.id.beforeBreakfastButton);
         beforeLunchButton = findViewById(R.id.beforeLunchButton);
         radioGroup = findViewById(R.id.rg);
+         ll=new Stack<>();
 
-        afterDinnerButton.setOnClickListener(v -> afterDinnerSelected = afterClickChange(afterDinnerButton, afterDinnerSelected));
-        afterBreakfastButton.setOnClickListener(v -> afterBreakfastSelected = afterClickChange(afterBreakfastButton, afterBreakfastSelected));
-        afterLunchButton.setOnClickListener(v -> afterLunchSelected = afterClickChange(afterLunchButton, afterLunchSelected));
-        beforeLunchButton.setOnClickListener(v -> beforeLunchSelected = afterClickChange(beforeLunchButton, beforeLunchSelected));
-        beforeBreakfastButton.setOnClickListener(v -> beforeBreakfastSelected = afterClickChange(beforeBreakfastButton, beforeBreakfastSelected));
-        beforeDinnerButton.setOnClickListener(v -> beforeDinnerSelected = afterClickChange(beforeDinnerButton, beforeDinnerSelected));
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+        mo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check=true;
+                DatePickerDialog datePickerDialog=new DatePickerDialog(
+                        EditReminder.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setListener1,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+
+            }
+        });
+        setListener1=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String date=day+"/"+month+"/"+year;
+                mo.setText(date);
+            }
+        };
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check=false;
+                DatePickerDialog datePickerDialog=new DatePickerDialog(
+                        EditReminder.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setListener2,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+
+
+            }
+        });
+    setListener2=new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            month=month+1;
+            String date1=day+"/"+month+"/"+year;
+            t1.setText(date1);
+        }
+    };
+        afterDinnerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                afterDinnerSelected = afterClickChange(afterDinnerButton, afterDinnerSelected);
+//                ll.add(afterDinnerButton.getText().toString());
+            }
+        });
+        afterBreakfastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                afterBreakfastSelected = afterClickChange(afterBreakfastButton, afterBreakfastSelected);
+//                ll.add(afterBreakfastButton.getText().toString());
+            }
+        });
+        afterLunchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                afterLunchSelected = afterClickChange(afterLunchButton, afterLunchSelected);
+
+            }
+        });
+        beforeLunchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beforeLunchSelected = afterClickChange(beforeLunchButton, beforeLunchSelected);
+            }
+        });
+        beforeBreakfastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beforeBreakfastSelected = afterClickChange(beforeBreakfastButton, beforeBreakfastSelected);
+            }
+        });
+        beforeDinnerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beforeDinnerSelected = afterClickChange(beforeDinnerButton, beforeDinnerSelected);
+
+            }
+        });
+
+        donereminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=firebaseAuth.getCurrentUser().getEmail();
+                String s=mname.getText().toString();
+                String s1=Integer.toString(selectedId);
+                String ms2=mo.getText().toString();
+                String ds3=t1.getText().toString();
+                if(s.isEmpty() || s1.equals("0") || ll.isEmpty() ||ms2.isEmpty() || ds3.isEmpty()){
+                    Toast.makeText(EditReminder.this, "Plz Fill all fields ", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                        Map<String, Object> mp = new HashMap<>();
+                        Map<String, Object> mp1 = new HashMap<>();
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("Type", s1);
+                        map.put("Time and Schedule", ll);
+                        map.put("Start Date", ms2);
+                        map.put("End Date", ds3);
+                        mp1.put("Medicine", s);
+                        mp1.put(s, map);
+                        mp.put("Email", email);
+                        mp.put("Medicine Names", mp1);
+                        mp.put("User Name", firebaseAuth.getCurrentUser().getDisplayName());
+                        mp.put("Phone", firebaseAuth.getCurrentUser().getPhoneNumber());
+
+                        firebaseFirestore.collection("Users").document(email).set(mp).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(EditReminder.this, "Data base updated", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+
+        });
+
     }
-
     @SuppressLint("ResourceAsColor")
     public boolean afterClickChange(Button button, boolean afterClick) {
         afterClick = !afterClick;
         if (afterClick == true) {
             button.getBackground().setAlpha(100);
+            ll.add(button.getText().toString());
+
         } else {
             button.getBackground().setAlpha(255);
+            ll.pop();
+
         }
         return afterClick;
     }
 
     public void onClickMethod(View v) {
-        int selectedId = radioGroup.getCheckedRadioButtonId();
+        selectedId = radioGroup.getCheckedRadioButtonId();
         genderradioButton = (RadioButton) findViewById(selectedId);
         if (lastButtonReference != null) {
             lastButtonReference.getBackground().setAlpha(255);
--- app/src/main/java/com/example/medapp/MedicinePage.java
+++ app/src/main/java/com/example/medapp/MedicinePage.java
@@ -10,9 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MedicinePage extends AppCompatActivity {
-    Button button;
+    Button ereminder,siout;
+    EditText e1,e2;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -24,13 +33,47 @@ public class MedicinePage extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         android.graphics.drawable.Drawable background = MedicinePage.this.getResources().getDrawable(R.drawable.color);
         getWindow().setBackgroundDrawable(background);
-        button = findViewById(R.id.button3);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        ereminder = findViewById(R.id.Editreminder);
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        siout=findViewById(R.id.sout);
+        ereminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+////                String s=firebaseAuth.getCurrentUser().getEmail();
+//                String s1=e1.getText().toString();
+//
+//                String fulname=e2.getText().toString();
+//                if(s1.isEmpty()){
+//                    Toast.makeText(MedicinePage.this, "Plz fill all feilds", Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//
+//                    Map<String,Object> map=new HashMap<>();
+//                    map.put("Medicine name",s1);
+//                    firebaseFirestore.collection("Users").document(fulname).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<Void> task) {
+//                            if(task.isSuccessful()){
+//                                Toast.makeText(MedicinePage.this, "DAta base added", Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                    });
+//                    Toast.makeText(MedicinePage.this, "Medicine mane is :-"+s1+" Email ", Toast.LENGTH_SHORT).show();
+//                }
                 Intent intent = new Intent(getApplicationContext(), EditReminder.class);
                 startActivity(intent);
+            }
+
+        });
+        siout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseAuth.signOut();
+                startActivity(new Intent(MedicinePage.this,LoginActivity.class));
+                finish();
+                Toast.makeText(MedicinePage.this, "Sign out", Toast.LENGTH_SHORT).show();
             }
         });
     }
